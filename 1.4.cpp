94425f38649fe99094bc5fd3d382dee288fa785d
Somnolency <somnolency@outlook.com>
2020-10-22 23:47:26 +0800

    C++编程第一次提交

---
+++ 1.4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string first_name,last_name;
+	cout<<"Please tell your name"<<endl;
+	cin>>first_name>>last_name;
+	cout<<"\nHolle ,"<<first_name <<" "<<last_name<<".\nNice to meet to you!";
+	return 0;
+}
+
---
+++ 1.5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char name[12];
+	cin.getline(name,12);
+	cout<<name;
+	return 0;
+}
